0566667edc1179941283d925ceea1575d82be5e3
Carol254 <wanguic49@gmail.com>
2023-04-12 22:14:11 +0300

    few changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
+
   title = 'fitnessTimerApp';
 
-  start_counting(){
-  
-  }
 
-  pause_counting(){
-  
-  }
 
-  stop_counting(){
-  
-  }
 
 }
 
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+
+
+
+  }
+
+
+  start_counting(){
+  
+  }
+
+  pause_counting(){
+  
+  }
+
+  stop_counting(){
+  
+  }
+
+}
